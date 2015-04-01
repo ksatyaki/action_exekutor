944496f1869725986721ce732c7bf9f55e49d305
Chittaranjan Srinivas <chitt@live.in>
2015-04-01 18:44:59 +0200

    License update

---
+++ action_exekutor.el
@@ -0,0 +1,5 @@
+; LISP CODE FOR PROJECT
+
+(setq ecb-source-path (quote ("/home/ace/chittaranjan_ros/catkin_ws3/src/action_exekutor")))
+(semantic-add-system-include "/home/ace/chittaranjan_ros/catkin_ws3/src/action_exekutor/include" 'c++-mode)
+(setq compile-command "cd /home/ace/chittaranjan_ros/catkin_ws3 && catkin_make")
